dd81e2eaab83a711f709387b6da248c12684ac06
MD Rakib Khan <contact.antor.bd@gmail.com>
2023-03-14 13:17:55 +0600

    branding

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Branding from '../Branding/Branding';
 
 const Home = () => {
     return (
         <>
             <Banner />
+            <Branding />
         </>
     );
 };
